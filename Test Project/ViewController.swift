3fa110da4aec09ea2756a7ece4c1aa4d7e3e6407
Vincent Frascello <vincent.frascello@gmail.com>
2020-12-13 11:51:57 -0800

    Initial Commit

---
+++ Test Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Project
+//
+//  Created by Vince Frascello on 12/13/20.
+//  Copyright © 2020 Vince Frascello. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Test ProjectTests/Test_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  Test_ProjectTests.swift
+//  Test ProjectTests
+//
+//  Created by Vince Frascello on 12/13/20.
+//  Copyright © 2020 Vince Frascello. All rights reserved.
+//
+
+import XCTest
+@testable import Test_Project
+
+class Test_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
